0407d10401f721ae8b9e0c1e7b73c62f8c7b8fe2
Kelsey Shinners <kshinns@gmail.com>
2021-02-09 22:05:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,8 @@
 function basicTeenager(age) {
-  if(age<=19 && age>=13){
-    return 'You are a teenager!'
-  }
-  else return undefined
-
+  if(age<=19 && age>=13)
+  {return 'You are a teenager!'}
+  else 
+  {return undefined}
 }
 
 function teenager(age) {
